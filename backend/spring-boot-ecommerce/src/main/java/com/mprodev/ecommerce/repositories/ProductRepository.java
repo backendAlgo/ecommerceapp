86e5829410628f75328253017ced015a6471f422
Mirshod <mirshod.mirjonov@mail.ru>
2021-04-25 02:40:56 +0500

    front beginning with react

--- backend/spring-boot-ecommerce/src/main/java/com/mprodev/ecommerce/repositories/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/mprodev/ecommerce/repositories/ProductRepository.java
@@ -3,9 +3,12 @@ package com.mprodev.ecommerce.repositories;
 import com.mprodev.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /* Mirshod created on 2/19/2021 */
 @Repository //it is not mandatory Spring realizes when extending JpaRepository
+@CrossOrigin("http://localhost:") // to make our javascript code in
+// web browser works. React or Angular
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
